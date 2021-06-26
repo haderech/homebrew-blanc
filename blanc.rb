810c25a8b5142cfd21597a59f501a3e63b2b53f4
Jeeyong Um <conr2d@gmail.com>
2021-06-26 21:28:45 +0900

    0.9.1 release

--- blanc.rb
+++ blanc.rb
@@ -2,8 +2,8 @@ class Blanc < Formula
 
    homepage "https://github.com/turnpike/blanc"
    revision 0
-   url "https://github.com/turnpike/blanc/archive/0.9.0.tar.gz"
-   version "0.9.0"
+   url "https://github.com/turnpike/blanc/archive/0.9.1.tar.gz"
+   version "0.9.1"
    
    option :universal
 
@@ -21,8 +21,8 @@ class Blanc < Formula
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/turnpike/blanc/releases/download/0.9.0"
-      sha256 big_sur: "a5812b88757b6a40b53cb2334db4847009b2612a8dceb7ede78629b18ccb38f8"
+      root_url "https://github.com/turnpike/blanc/releases/download/0.9.1"
+      sha256 big_sur: "33879a6a1bdcdfe7f4e11ac6a50a56d70ff3ccb6dab11b1ad5b9c97b7e32c882"
    end
    def install
       raise "Error, only supporting binary packages at this time"
