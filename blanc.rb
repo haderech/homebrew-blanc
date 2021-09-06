5d36628a0a7eb1cbf32f69a319006181a6a39a2c
Jeeyong Um <conr2d@gmail.com>
2021-09-06 19:16:59 +0900

    0.10.0 release

--- blanc.rb
+++ blanc.rb
@@ -2,27 +2,20 @@ class Blanc < Formula
 
    homepage "https://github.com/turnpike/blanc"
    revision 0
-   url "https://github.com/turnpike/blanc/archive/0.9.2.tar.gz"
-   version "0.9.2"
+   url "https://github.com/turnpike/blanc/archive/0.10.0.tar.gz"
+   version "0.10.0"
    
    option :universal
 
    depends_on "cmake" => :build
-   depends_on "automake" => :build
-   depends_on "libtool" => :build
-   depends_on "wget" => :build
-   depends_on "gmp" => :build
-   depends_on "gettext" => :build
-   depends_on "doxygen" => :build
-   depends_on "graphviz" => :build
-   depends_on "lcov" => :build
+   depends_on "llvm" => :build
    depends_on :xcode => :build
    depends_on :macos => :catalina
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/turnpike/blanc/releases/download/0.9.2"
-      sha256 big_sur: "619123e856de8f06368dd97788062f6baeebf3114954d7a38389a0781bc2a877"
+      root_url "https://github.com/turnpike/blanc/releases/download/0.10.0"
+      sha256 big_sur: "b471c8d239c7f0629aa776280310c86b8535b17888186efea201df1f4fae27d9"
    end
    def install
       raise "Error, only supporting binary packages at this time"
