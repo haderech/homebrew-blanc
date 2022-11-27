405c685a00414686f24a376192fd50a301267816
Jeeyong Um <conr2d@proton.me>
2022-11-28 02:16:06 +0900

    0.12.1 release

--- blanc.rb
+++ blanc.rb
@@ -2,8 +2,8 @@ class Blanc < Formula
 
   homepage "https://github.com/haderech/blanc"
   revision 0
-  url "https://github.com/haderech/blanc/archive/0.12.0.tar.gz"
-  version "0.12.0"
+  url "https://github.com/haderech/blanc/archive/0.12.1.tar.gz"
+  version "0.12.1"
 
   option :universal
 
@@ -11,12 +11,12 @@ class Blanc < Formula
   depends_on "cmake"
   depends_on "llvm"
   depends_on :xcode => :build
-  depends_on :macos => :monterey
+  depends_on :macos => :ventura
 
   bottle do
-    root_url "https://github.com/haderech/blanc/releases/download/0.12.0"
-    sha256 arm64_monterey: "746486277dbf6c8276846d4ce5b5efdf38a8a1f15b62990fcef97c6ef4f3211f"
-    sha256 monterey:       "f69661864e7572fc31c88717ce79f07835f3cd6b36b20528b3789e8097e184d3"
+    root_url "https://github.com/haderech/blanc/releases/download/0.12.1"
+    sha256 arm64_ventura: "69a182d6eeed9b6c1b745228a331557719d4399eff3f4c5fe791c4bfb9d2670f"
+    sha256 ventura:       "d5d90097906eba8c25d480cc37eb41ea335c6d92acac58ed4f9d59c5645e71b5"
   end
   def install
     raise "Error, only supporting binary packages at this time"
